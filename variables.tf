840090a13ede5abc9a3e28586ded2f8ed806f764
Jose Rivera <joserivera4497@gmail.com>
2019-02-26 14:10:33 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,3 +13,30 @@ variable "region" {
 variable "latest_snapshot" {
     default = "snap-07672819ca1b5b04c"
 }
+
+variable "db_allocated_storage" {
+	default = 20
+}
+
+variable "db_storage_type" {
+	default = "gp2"
+}
+
+variable "db_engine" {
+	default  = "mysql"
+}
+
+variable "db_engine_version" {
+	default = "8.0.13"
+}
+variable "db_instance_class" {
+	default  = "db.t2.micro"
+}
+
+variable "db_name" {
+	default  = "newdb"
+}
+
+variable "db_parameter_group_name" {
+	default = "default.mysql8.0"
+}
