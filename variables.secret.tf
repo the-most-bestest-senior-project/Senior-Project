8a0f6a32a2502d140bc45998f09ede4599544592
Jose Rivera <joserivera4497@gmail.com>
2019-02-26 14:10:13 -0500

    Update variables.secret.tf

--- variables.secret.tf
+++ variables.secret.tf
@@ -7,3 +7,12 @@ variable "aws_secret_key" {
  description = "The AWS secret key."
  default     = ""
 }
+
+variable "db_username" {
+	description = "Username for DB"
+	default  = ""
+}
+variable "db_password" {
+	description = "Password for DB"
+	default   = ""
+}
