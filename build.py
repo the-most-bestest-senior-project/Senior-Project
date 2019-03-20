7c6c2b70b00db8760f21b0f0fa2c78b641a999e2
unknown <curleykatie1@gmail.com>
2019-03-20 13:16:30 -0400

    Fixed repo; updated folders

--- build.py
+++ build.py
@@ -1,8 +1,10 @@
 import boto3
+import subprocess
+import sys
 
 client = boto3.client('ec2', 
-        aws_access_key_id=key,
-        aws_secret_access_key=secret,
+        aws_access_key_id="",
+        aws_secret_access_key="",
         region_name='us-east-1')
 
 #prints out our snapshots
@@ -10,4 +12,6 @@ print(client.describe_snapshots(OwnerIds=['self']))
 
 #change this shit later when we get the database up
 #will be super ez to grab the latest snapshot 
+
+subprocess.call([sys.executable, './setup.sh', 'ec2'], shell=True)
 
--- variables.secret.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key" {
- description = "The AWS access key."
- default     = ""
-}
-
-variable "aws_secret_key" {
- description = "The AWS secret key."
- default     = ""
-}
