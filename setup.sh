54f62977b4aa82f265df53662eb9e8d4a50b1052
Stephanie Nash <snash@ufl.edu>
2019-04-17 20:16:52 -0400

    not sure why it's mad about fi

--- setup.sh
+++ setup.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/sh
-
 DIR=""
 SNAP=""
-if [ $1 -ne 'ec2' ]
-  then
+if [ $1 != "ec2" ]
+then
     cd tf/rds
     #Runs terraform scripts to setup rds and vpc networking
     terraform init
     terraform plan\
-    -var 'db_username'=$2
-    -var 'db_password'=$3
-    -var 'aws_access_key'=$4
-    -var 'aws_secret_key'=$5
+    -var 'db_username='$2 \
+    -var 'db_password='$3 \
+    -var 'aws_access_key='$4 \
+    -var 'aws_secret_key='$5 \
     -out out.terraform
     terraform apply out.terraform
     rm out.terraform KEY
-  fi
 else
     DIR=$1
     SNAP=$2
-
     cd tf/$DIR
     #Runs terraform scripts to setup ec2
     terraform init
