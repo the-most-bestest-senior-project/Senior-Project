6d0eb9371ee1a475c52f8f44344e6a25e524b25c
unknown <curleykatie1@gmail.com>
2019-04-19 11:11:15 -0400

    Updated temp with gui changes

--- build.py
+++ build.py
@@ -1,13 +1,91 @@
 import boto3
+import subprocess
+import pymysql
+import sys
+import datetime
+import os
 
-client = boto3.client('ec2', 
-        aws_access_key_id=key,
-        aws_secret_access_key=secret,
-        region_name='us-east-1')
+def main():
+        print('build.py was called')
+        access_key=sys.argv[2]
+        secret_key=sys.argv[3]
 
-#prints out our snapshots
-print(client.describe_snapshots(OwnerIds=['self']))
+        ec2 = boto3.client('ec2',
+                aws_access_key_id=access_key,
+                aws_secret_access_key=secret_key,
+                region_name='us-east-1')
 
-#change this shit later when we get the database up
-#will be super ez to grab the latest snapshot 
+        ec2_r = boto3.resource('ec2',
+                aws_access_key_id=access_key,
+                aws_secret_access_key=secret_key,
+                region_name='us-east-1')
 
+        filters = [{'Name':'tag:Name', 'Value':'main'}]
+
+        response = ec2.describe_vpcs(
+                Filters=[
+                        {
+                        'Name': 'tag:Name',
+                        'Values': [
+                                "main",
+                        ]
+                        }
+                ]
+        )
+        vpc_id = ""
+        resp = response['Vpcs']
+        for i in resp:
+                for k, v in i.items():
+                        if k == "VpcId":
+                                vpc_id = v
+        cnt = 0
+        for vpc in ec2_r.vpcs.all():
+                if vpc.id == vpc_id:
+                        for subnet in vpc.subnets.all():
+                                if cnt == 0:
+                                        sub = subnet.id
+                                        break
+                                cnt+= 1
+        rds = boto3.client('rds',
+                aws_access_key_id=access_key,
+                aws_secret_access_key=secret_key,
+                region_name='us-east-1')
+
+        addr=""
+        usr=""
+        pwd=sys.argv[1]
+
+        rds_instances = rds.describe_db_instances()
+        for i in rds_instances['DBInstances']:
+                arn = i['DBInstanceArn']
+                tags = rds.list_tags_for_resource(ResourceName=arn)['TagList']
+                tag = next(iter(filter(lambda tag: tag['Key'] == 'Name' and tag['Value'] == 'gaming_db', tags)), None)
+                if tag:
+                        addr = i.get('Endpoint').get('Address')
+                        usr = i.get('MasterUsername')
+
+        db = pymysql.connect(host=addr,
+                       port=3306,
+                       user=usr,
+                       password=pwd) #figure this out
+
+        snap=""
+        cur = db.cursor()
+        cur.execute("USE `senior_design`;")
+        #sql_insert_query = "INSERT INTO `ebs_table`(`user_ID`, `snapshot`, `snapshot_time`, `AMI`) VALUES (%s,%s,%s,%s)"
+        #insert_tuple = ("123", "123", datetime.datetime.now(), "idk")
+        #cur.execute(sql_insert_query, insert_tuple)
+
+        cur.execute("SELECT `snapshot` FROM `ebs_table` ORDER BY `snapshot_time` DESC LIMIT 1")
+        snap = cur.fetchone()
+        print(snap)
+        for row in cur:
+                print(row)
+
+        db.commit()
+        db.close()
+
+        os.system("cd tf/ec2 && terraform init && terraform apply -var 'aws_access_key='%s -var 'aws_secret_key='%s -var 'latest_snapshot='%s -var 'vpc_id='%s -var 'subnet_id='%s -auto-approve" % (access_key, secret_key, snap, vpc_id, sub))
+        #subprocess.call('setup.sh ec2 "%s" "%s" "%s"' %(snap, vpc_id, sub), shell=True)
+
+main()
--- destroy.py
+++ destroy.py
@@ -1,24 +1,78 @@
 import boto3
+import subprocess
+import pymysql
+import sys  
+import datetime
+import time
+import os
 
-volumes = []
+def get_aws_account_id(access_key, secret_key):
+    sts = boto3.client(
+        "sts", aws_access_key_id=access_key, aws_secret_access_key=secret_key,
+    )
+    user_arn = sts.get_caller_identity()["Arn"]
+    return user_arn.split(":")[4]
+
+access_key = sys.argv[2]
+secret_key = sys.argv[3]
 
 #Creates snapshot from running gaming instance
 ec2 = boto3.resource('ec2')
+account_id = get_aws_account_id(access_key, secret_key)
+
+ami = "ami-0c9229763022bca45"
+snapshot = ""
+
 for instance in ec2.instances.filter(
     Filters=[
         {'Name': "instance-state-name", 'Values': ["running"]},
-        {'Name': "image-id", 'Values': ["ami-017dbf6a"]}]
+        {'Name': "image-id", 'Values': ["ami-0c9229763022bca45"]}]
 ):
+    #need to stop the instance in order to take snapshot of root volume
+    instance.stop()
     for device in instance.block_device_mappings:
-        volumes.append(device)
+        snapshot = ec2.create_snapshot(VolumeId=device.get('Ebs').get('VolumeId')).id
 
-#Check if xvdf volume exists and take snapshot of it, otherwise this is the first boot
-#so we take a snapshot of the root device
-if not any(device['DeviceName'] == '/dev/xvdf' for device in volumes):
-    ec2.create_snapshot(VolumeId=device.get('Ebs').get('VolumeId'))
-else:
-    if device['DeviceName'] == '/dev/xvdf':
-        print(device['Ebs']['VolumeId'])
-        ec2.create_snapshot(VolumeId=device.get('Ebs').get('VolumeId'))
+rds = boto3.client('rds', 
+                aws_access_key_id=access_key,
+                aws_secret_access_key=secret_key,
+                region_name='us-east-1')
+
+addr=""
+usr=""
+pwd=sys.argv[1]
+
+rds_instances = rds.describe_db_instances()
+for i in rds_instances['DBInstances']:
+    arn = i['DBInstanceArn']
+    tags = rds.list_tags_for_resource(ResourceName=arn)['TagList']
+    tag = next(iter(filter(lambda tag: tag['Key'] == 'Name' and tag['Value'] == 'gaming_db', tags)), None)
+    if tag:
+        addr = i.get('Endpoint').get('Address') 
+        usr = i.get('MasterUsername')
+
+db = pymysql.connect(host=addr,
+                    port=3306,
+                    user=usr,      
+                    password=pwd)
+
+cur = db.cursor()
+
+ts = time.time()
+timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+#TODO: fix date column, not inserting correctly
+cur.execute("USE `senior_design`;")
+sql_insert_query = "INSERT INTO `ebs_table`(`user_ID`, `snapshot`, `snapshot_time`, `AMI`) VALUES (%s,%s,%s,%s)"
+insert_tuple = (account_id, snapshot, timestamp, ami)
+cur.execute(sql_insert_query, insert_tuple)
+cur.execute("SELECT * FROM `ebs_table`;")
+for row in cur:
+    print(row)
+
+db.commit()
+db.close()
+
+#TODO: call terraform destroy on ec2
+os.system('cd tf/ec2 && terraform init && terraform destroy -force')
             
-        
